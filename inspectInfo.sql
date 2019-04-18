e46013bdd08aa8acb7e4938a8bfde42933c530e8
yizhou <1970142250@qq.com>
2019-04-18 14:39:44 +0800

    sql file

---
+++ inspectInfo.sql
@@ -0,0 +1,18 @@
+CREATE TABLE InspectInfo (
+	id varchar (255) NOT NULL,
+	EnrollNumber varchar(255) NULL,
+	userName varchar(255) NULL,
+	userInfo varchar(255) NULL,
+	VerifyMode int NULL,
+	InOutMode int NULL,
+	Times varchar(255) NULL,
+	Year int NULL,
+	Month int NULL,
+	Day int NULL,
+	Hour int NULL,
+	Minute int NULL,
+	Second int NULL,
+	sortTime datetime NULL,
+	mip varchar(255) NULL,
+	countTime datetime NULL
+	)
